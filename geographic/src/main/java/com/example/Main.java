5b60051b0f86a9dd2bc7691c8818dc2a89a31f3a
Sunshower1260 <155897796+Sunshower1260@users.noreply.github.com>
2024-01-30 12:39:12 +0700

    Add files via upload

---
+++ geographic/src/main/java/com/example/Main.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import com.example.Controller.ManageEastAsiaCountries;
+
+public class Main {
+    public static void main(String[] args) {
+        new ManageEastAsiaCountries().run();
+    }
+}
